45a15f49e743ba1f82a36656d915010dafdecc28
rudi <rudi@bukuku.solutions>
2022-11-08 14:58:55 +0700

    fist

---
+++ app/Http/Controllers/TelenjarController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Proxiers\ProxyService;
+use Illuminate\Http\Request;
+
+class TelenjarController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request, ProxyService $proxyService)
+    {
+        if ($request->filled('TOKEN') ||
+            $request->filled('PLN') ||
+            $request->filled('NONTALGIS') ||
+            $request->filled('TELKOM') ||
+            $request->filled('BPJS') ||
+            $request->filled('MULTIFINANCE')
+        ) {
+            return match ($request->TOKEN) {
+                'INQ' => $proxyService->inquiry(
+                    $request->trxid,
+                    $request->produk,
+                    $request->tujuan,
+                    $request->nominal
+                ),
+                'PAY' => $proxyService->pay(
+                    $request->trxid,
+                    $request->produk,
+                    $request->tujuan,
+                    $request->respid,
+                    $request->nominal
+                ),
+                default => throw new \Exception('Invalid parameter ' . $request->TOKEN),
+            };
+        } elseif ($request->filled('PBB')) {
+            // implementasi untuk PPOB
+            return [];
+        }
+
+        return $proxyService->purchase(
+            $request->trxid,
+            $request->produk,
+            $request->tujuan,
+            $request->nominal
+        );
+    }
+}
